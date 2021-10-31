b2e270512ea0c71c17ca04ee2c9bf64f44cf2c8e
yna <yash.nagar@iviewlabs.net>
2021-10-31 22:02:11 +0530

    Followup

---
+++ backend/helpers/database.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+// sequelize connect
+exports.getSqlConnection = (config) => {
+    let seqInstance = new Sequelize(config.database, config.username, config.password, {
+        host: config.host,
+        port: config.port,
+        dialect: 'postgres',
+        timezone: '+00:00', //Default `+00:00` to manage data as per timezone
+    });
+
+    seqInstance
+        .authenticate()
+        .then(() => {
+            logger.info('Connection has been established successfully.');
+        })
+        .catch((error) => {
+            logger.error('Unable to connect to the database:', error);
+            throw error;
+        });
+
+    seqInstance = {
+        Sequelize: Sequelize,
+        seqInstance: seqInstance,
+    };
+    return seqInstance;
+};
